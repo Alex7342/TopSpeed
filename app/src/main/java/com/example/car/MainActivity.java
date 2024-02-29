98079435635548a15027ee80ea2c58fafabd8f2f
Florin <florinelgaina@yahoo.com>
2024-02-29 19:17:35 +0200

    refactoring + toasts

--- app/src/main/java/com/example/car/MainActivity.java
+++ app/src/main/java/com/example/car/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -13,6 +13,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
@@ -23,7 +24,21 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     private long endTime;
     private boolean hasChanged;
     private boolean hasFinished;
+    
+    private static final int locationRequestCode = 1;
+    private static final int minimumUpdateTime= 80; // ms
+    private static final int minimumUpdateDistance = 3; //m
 
+    private void requestPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            String[] permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, locationRequestCode);
+        }
+        else
+            Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show();
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,27 +49,31 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         speedSeekBar = findViewById(R.id.speedSeekBar);
         hasChanged = false;
         hasFinished = false;
-
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
-            //TODO Get permissions
-            /*
-            ActivityCompat.requestPermissions(LoginActivity.this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CALL);
-
-            ActivityCompat.requestPermissions(this,);
-            */
-            return;
+        
+        requestPermissions();
+        
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minimumUpdateTime, minimumUpdateDistance, this);
         }
-
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5, 0, this);
-        this.onLocationChanged((Location) null);
     }
 
     private String computeTimeElapsed() {
-        double timeElapsed = (double) ((endTime - startTime) / 1000.0);
+        double timeElapsed = ((endTime - startTime) / 1000.0);
         return Double.toString(timeElapsed);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == locationRequestCode){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permissions Granted", Toast.LENGTH_SHORT).show();
+            }
+            else Toast.makeText(this, "Permissions Denied", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
