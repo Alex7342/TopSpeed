dd20c542826699fc0f941aff25f0f5b67727e4a7
Florin <florinelgaina@yahoo.com>
2024-03-01 10:44:08 +0200

    gps provider + refactoring

--- app/src/main/java/com/example/car/MainActivity.java
+++ app/src/main/java/com/example/car/MainActivity.java
@@ -15,19 +15,30 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity{
 
     private TextView speedTextView;
     private TextView resultTextView;
     private SeekBar speedSeekBar;
+
+    private TextView timeElapsedView;
     private long startTime;
     private long endTime;
     private boolean hasChanged;
     private boolean hasFinished;
     
     private static final int locationRequestCode = 1;
-    private static final int minimumUpdateTime= 80; // ms
-    private static final int minimumUpdateDistance = 3; //m
+    private static final int minimumUpdateTime= 50; // ms
+    private static final int minimumUpdateDistance = 0; //m
+
+    private void initialise(){
+        hasChanged = false;
+        hasFinished = false;
+
+        speedTextView.setText("0 km/h");
+        speedSeekBar.setProgress(0);
+
+    }
 
     private void requestPermissions(){
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
@@ -47,20 +58,43 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         speedTextView = findViewById(R.id.speedTextView);
         resultTextView = findViewById(R.id.resultTextView);
         speedSeekBar = findViewById(R.id.speedSeekBar);
+        timeElapsedView = findViewById(R.id.timeElapsedView);
         hasChanged = false;
         hasFinished = false;
         
         requestPermissions();
+        initialise();
         
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
+
             LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minimumUpdateTime, minimumUpdateDistance, this);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minimumUpdateTime, minimumUpdateDistance, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    int speed = (int) (location.getSpeed() * 3.6);
+                    speedTextView.setText(speed + " km/h");
+                    speedSeekBar.setProgress(speed);
+
+                    if (!hasChanged) {
+                        hasChanged = true;
+                        startTime = System.currentTimeMillis();
+                    }
+
+                    if (speed >= 100 && !hasFinished) {
+                        hasFinished = true;
+                        endTime = System.currentTimeMillis();
+                        resultTextView.setText("Result: " + computeTimeElapsed() + " s");
+                    }
+
+                    timeElapsedView.setText(Double.toString(Math.round(System.currentTimeMillis() - startTime / 1000.0)) + " s");
+                }
+            });
         }
     }
 
     private String computeTimeElapsed() {
-        double timeElapsed = ((endTime - startTime) / 1000.0);
+        double timeElapsed = Math.round((endTime - startTime) / 1000.0);
         return Double.toString(timeElapsed);
     }
 
@@ -74,51 +108,5 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             }
             else Toast.makeText(this, "Permissions Denied", Toast.LENGTH_SHORT).show();
         }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        if (location == null) {
-            speedTextView.setText("0");
-            speedSeekBar.setProgress(0);
-        }
-        else {
-            int speed = (int) (location.getSpeed() * 3.6);
-            speedTextView.setText(Integer.toString(speed) + " km/h");
-            speedSeekBar.setProgress(speed);
-
-            if (!hasChanged) {
-                hasChanged = true;
-                startTime = System.currentTimeMillis();
-            }
-
-            if (speed >= 100 && !hasFinished) {
-                hasFinished = true;
-                endTime = System.currentTimeMillis();
-                resultTextView.setText("Result: " + computeTimeElapsed() + "seconds");
-            }
-        }
-    }
-
-
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        //TODO
     }
 }
